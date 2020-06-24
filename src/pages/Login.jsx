2add21cb69ebc0149947ffe9f428efc9e4d73d02
Henrique Rezende <henriquerezendec@gmail.com>
2020-06-24 18:28:31 -0300

    Starting login and adding materialize

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="row">
+      <input type="text" className="col s10 offset-s1" />
+      <input type="text" className="col s10 offset-s1" />
+      <button type="button" className="col s10 offset-s1 waves-effect waves-light btn">Entrar</button>
+    </div>
+  );
+};
+
+export default Login;
