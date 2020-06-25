c31735b15ab2e97a2f332142dd68fb863b620726
DevT <aguiarthiag@gmail.com>
2020-06-25 17:43:48 -0300

    CC

--- src/pages/DrinkRecipes.jsx
+++ src/pages/DrinkRecipes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const DrinkRecipes = () => ( 
+const DrinkRecipes = () => (
   <div>
     <Header title="Bebidas" searchEnabled />
     <Footer />
