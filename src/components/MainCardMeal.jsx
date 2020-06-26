316068454a7d009f4b0eacb450d1c8fa8a8d0eed
mirellaalves <mirella.alves@gmail.com>
2020-06-26 10:24:33 -0300

    Drafts.

--- src/components/MainCardMeal.jsx
+++ src/components/MainCardMeal.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const MainCardMeal = () =>
-  <div>
-    <img data-testid={`${index}-card-img`} alt="Meal Photo" className="meal-image" src={strMealThumb} />
-    <h4 data-testid={`${index}-card-name`} className="meal-title">{strMeal}</h4>
-  </div>;
+const MainCardMeal = () => {
+  const { meal } = this.props;
+  const { index, strMealThumb, strMeal, idMeal } = meal;
+
+  return (
+    <div>
+      <img data-testid={`${index}-card-img`} alt="Meal Photo" className="meal-image" src={strMealThumb} />
+      <h4 data-testid={`${index}-card-name`} className="meal-title">{strMeal}</h4>
+      <Link className="meal-card-details" to={`/comidas/${idMeal}`}>VER DETALHES</Link>
+    </div>
+  );
+}
 
 export default MainCardMeal;
--- src/pages/FoodRecipes.jsx
+++ src/pages/FoodRecipes.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import MainCardMeal from '../components/MainCardMeal';
 
-const FoodRecipes = () => <div><Header title="Comidas" searchEnabled /></div>;
+const FoodRecipes = () => (
+  <div>
+    <Header title="Comidas" searchEnabled />
+    <div>
+      {meals.map((meal) => <MainCardMeal key={meal.idMeal} meal={meal} />)}
+    </div>
+  </div>
+);
 
 export default FoodRecipes;
