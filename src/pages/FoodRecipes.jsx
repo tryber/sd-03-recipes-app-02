ad73f22282af9788003773e2ca96c6ab42a06acb
mirellaalves <mirella.alves@gmail.com>
2020-06-26 20:00:31 -0300

    Correcting FoodRecipes page.

--- src/pages/FoodRecipes.jsx
+++ src/pages/FoodRecipes.jsx
@@ -5,19 +5,13 @@ import * as fetchMealAPI from '../services/fetchMealAPI';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MainCardMeal from '../components/MainCardMeal';
-import MainCardDrink from '../components/MainCardDrink';
-
-const getCards = () =>
-  (title="Comidas")
-  ? (meals.slice(0, 12).map((meal) => <MainCardMeal key={meal.idMeal} meal={meal} />))
-  : (drinks.slice(0, 12).map((drink) => <MainCardDrink key={drink.idDrink} drink={drink} />));
 
 const FoodRecipes = () => {
   return (
     <div>
       <Header title="Comidas" searchEnabled />
       <div>
-        {getCards()}
+        {meals.slice(0, 12).map((meal) => <MainCardMeal key={meal.idMeal} meal={meal} />)}
       </div>
       <Footer />
     </div>
