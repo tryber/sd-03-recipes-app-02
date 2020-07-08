6e9bf361ef9a07dfcc73b79976ea267881ac5c54
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-08 20:11:51 -0300

    Resolving conflict

--- src/pages/ExploreRecipes.jsx
+++ src/pages/ExploreRecipes.jsx
@@ -9,29 +9,35 @@ const ExploreRecipes = ({ title, url, type, history }) => {
   const handleClick = () => {
     let idType = 'idDrink';
     if (type === 'meal') idType = 'idMeal';
-    getRandomRecipe(type)
-      .then((data) => history.push(`/${url}/${(data.drinks || data.meals)[0][idType]}`));
+    getRandomRecipe(type).then((data) =>
+      history.push(`/${url}/${(data.drinks || data.meals)[0][idType]}`),
+    );
   };
   return (
     <div style={{ marginTop: '70px' }}>
       <Header title={title} searchEnabled={false} />
       <div className="explore">
         <Link to={`/explorar/${url}/ingredientes`} data-testid="explore-by-ingredient">
-          <button className="btn explore-btn" type="button">Por Ingredientes</button>
+          <button className="btn explore-btn" type="button">
+            Por Ingredientes
+          </button>
         </Link>
-        {type === 'meal' ?
+        {type === 'meal' ? (
           <Link to={'/explorar/comidas/area'} data-testid="explore-by-area">
-            <button className="btn explore-btn" type="button">Por Local de Origem</button>
+            <button type="button" className="btn explore-btn">
+              Por Local de Origem
+            </button>
           </Link>
-      :
-        null
-      }
+        ) : null}
         <button
-          className="btn explore-btn" type="button" data-testid="explore-surprise"
+          className="btn explore-btn"
+          type="button"
+          data-testid="explore-surprise"
           onClick={handleClick}
         >
-        Me Surpreenda!
-      </button>
+          Me Surpreenda!
+        </button>
+      </div>
       <Footer />
     </div>
   );
